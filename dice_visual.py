6760d560fcacf6de7f677646aeb30c8b8e4a512b
Ziyike1 <tul51428@temple.edu>
2024-10-23 10:06:04 +0800

    finish study, success create the bar-image and ready to download

--- dice_visual.py
+++ dice_visual.py
@@ -1,18 +1,20 @@
 import plotly.express as px
 from die import Die
 
-#创建一个D6
-die = Die()
+#创建两个D6
+die_1 = Die()
+die_2 = Die()
 
 #掷几次骰子并将结果储存在一个列表中
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 #分析结果
 frequencies = []
-poss_value = range(1, die.sides + 1)
+max_results = die_1.sides + die_2.sides
+poss_value = range(1, max_results + 1)
 for value in poss_value:
     frequency = results.count(value)
     frequencies.append(frequency)
@@ -20,7 +22,11 @@ for value in poss_value:
 # print(frequencies)
 
 #对结果进行可视化
-title = "Result of Rolling One D6 1000 Times"
+title = "Result of Rolling Two D6 1000 Times"
 labels = {'x':'Result', 'y':'Frequency of Result'}
 fig = px.bar(x=poss_value, y=frequencies,title=title,labels=labels)
+
+#进一步定制图形
+fig.update_layout(xaxis_dtick=1)
+
 fig.show()
--- dice_visual_d6d10.py
+++ dice_visual_d6d10.py
@@ -1,18 +1,20 @@
 import plotly.express as px
 from die import Die
 
-#创建一个D6
-die = Die()
+#创建D6, D10
+die_1 = Die()
+die_2 = Die(10)
 
 #掷几次骰子并将结果储存在一个列表中
 results = []
-for roll_num in range(1000):
-    result = die.roll()
+for roll_num in range(50000):
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 #分析结果
 frequencies = []
-poss_value = range(1, die.sides + 1)
+max_results = die_1.sides + die_2.sides
+poss_value = range(1, max_results + 1)
 for value in poss_value:
     frequency = results.count(value)
     frequencies.append(frequency)
@@ -20,7 +22,12 @@ for value in poss_value:
 # print(frequencies)
 
 #对结果进行可视化
-title = "Result of Rolling One D6 1000 Times"
+title = "Result of Rolling One D6 and One D10 1000 Times"
 labels = {'x':'Result', 'y':'Frequency of Result'}
 fig = px.bar(x=poss_value, y=frequencies,title=title,labels=labels)
+
+#进一步定制图形
+fig.update_layout(xaxis_dtick=1)
+
 fig.show()
+# fig.write_html('dice_visual_d6d.html')
